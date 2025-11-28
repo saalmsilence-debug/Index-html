280e074ccdace0982bf2cad51e3b06821b39f995
Saalmx solns <saalmsilence@gmail.com>
2025-11-28 09:44:41 +0300

    Update Service - worker.js

--- Service - worker.js
+++ Service - worker.js
@@ -1,1 +1,20 @@
+const CACHE_NAME = 'book7-cache-v2'; // Incrementing cache version for update
+const ASSETS = [
+  '/Index-html/',
+  '/Index-html/index.html',
+  '/Index-html/manifest.json'
+];
 
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+    return response || fetch(event.request);
+    })
+  );
+});
